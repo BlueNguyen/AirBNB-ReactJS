b794ad0e4121908f8d8ef7b0ed8e40bed9081a56
BlueNguyen <143895447+BlueNguyen@users.noreply.github.com>
2024-04-20 19:57:15 +0700

    modify

--- src/components/admin/ChartAdmin/ChartAdmin.jsx
+++ src/components/admin/ChartAdmin/ChartAdmin.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import ChartPie from "./ChartPie";
-// import ChartLine from "./ChartLine";
+import ChartPie from "./ChartPie";
+import ChartLine from "./ChartLine";
 
 const ChartAdmin = () => {
   return (
     <div className="grid gap-6 mb-8 md:grid-cols-2">
-      {/* <ChartPie />
-      <ChartLine /> */}
+      <ChartPie />
+      <ChartLine />
     </div>
   );
 };
