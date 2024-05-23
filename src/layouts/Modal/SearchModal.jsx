e904a582abadcf67afe0a60ad02f20f51ea5d5c3
anhtruyenb5 <anhtruyenb5@gmail.com>
2024-05-23 20:02:36 +0700

    sua Tim kiem

--- src/layouts/Modal/SearchModal.jsx
+++ src/layouts/Modal/SearchModal.jsx
@@ -5,74 +5,70 @@ import InputCustom from '../../components/user/Input/InputCustome';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllViTri } from '../../hooks/useGetAllViTri';
 import { locationServ } from '../../api/api';
+import { useNavigate } from 'react-router-dom';
 
 const SearchModal = () => {
     const [filteredResults, setFilteredResults] = useState([]);
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [showResults, setShowResults] = useState(false);
     const { arrViTri } = useSelector(state => state.useGetAllViTri)
+    const navigate = useNavigate();
     const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getAllViTri())
+    }, [dispatch])
 
-    const handleSearch = async (values) => {
-        try {
-            const data = await dispatch(getAllViTri());
-            setFilteredResults(data.payload);
-        } catch (error) {
-            console.error("Error fetching location data:", error);
-        }
-    };
 
-    const handleFocusSearch = () => {
-        setIsDropdownOpen(true);
-    };
-
-    const handleBlurSearch = () => {
-        setIsDropdownOpen(false);
-    };
-
-    const { handleSubmit, handleChange, handleBlur, values, touched, errors } = useFormik({
+    const formik = useFormik({
         initialValues: {
             searchTerm: "",
         },
         onSubmit: (values) => {
-            handleSearch(values)
+
         },
     });
 
     useEffect(() => {
-        console.log(filteredResults)
-    }, [values.searchTerm])
+        filterData(formik.values.searchTerm);
+    }, [formik.values.searchTerm]);
+
+    const filterData = (searchTerm) => {
+        const filtered = arrViTri.filter(item => {
+            const tenViTriLowerCase = item.tenViTri.toLowerCase();
+            const searchTermLowerCase = searchTerm.toLowerCase();
+            return tenViTriLowerCase.includes(searchTermLowerCase);
+        });
+        setFilteredResults(filtered)
+        setShowResults(true);
+    }
+
     return (
-        <form onSubmit={handleSubmit} className="relative">
+        <form onSubmit={formik.handleSubmit}>
             <div className='flex justify-center items-center'>
                 <div className='border-1[px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
                     <div className='flex flex-row items-center justify-between'>
-                        <div className='text-sm font-semibold px-6'>
+                        <div className='text-sm font-semibold px-6 relative'>
                             <InputCustom
                                 name="searchTerm"
                                 label="Địa điểm"
-                                onChange={handleChange}
-                                value={values.searchTerm}
+                                onChange={formik.handleChange}
+                                value={formik.values.searchTerm}
                                 id="searchTerm"
-                                error={errors.searchTerm}
-                                touched={errors.searchTerm}
                                 border={false}
-                                onBlur={handleBlurSearch}
-                                onFocus={handleFocusSearch}
                             />
                         </div>
                         <div className='hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center'>
                             <InputCustom
                                 label="Nhận phòng"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                                 border={false}
                             />
                         </div>
                         <div className='hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center'>
                             <InputCustom
                                 label="Trả phòng"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                                 border={false}
                             />
                         </div>
@@ -80,8 +76,8 @@ const SearchModal = () => {
                             <div className='hidden sm:block'>
                                 <InputCustom
                                     label="Thêm khách"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
                                     border={false}
                                 />
                             </div>
@@ -91,18 +87,26 @@ const SearchModal = () => {
                                 </button>
                             </div>
                         </div>
-                        <div className="absolute z-10 mt-1 w-1/4 bg-white border rounded-md shadow-lg top-20 left-0">
-                            {/* {isDropdownOpen && filteredResults.length > 0 && filteredResults.map((result, index) => (
-                                <div
-                                    key={index}
-                                    className="px-4 py-2 hover:bg-gray-100 cursor-pointer border-b"
-                                >
-                                    <div className=''>
-                                        {result.tinhThanh}
+                        {showResults && (
+                            <div className="absolute z-10 mt-1 w-1/4 bg-white border rounded-md shadow-lg top-20 left-0"
+                                onBlur={() => setShowResults(false)}
+                            >
+                                {/* Nội dung của div */}
+                                {filteredResults.length > 0 && filteredResults.map((result, index) => (
+                                    <div
+                                        key={index}
+                                        className="px-4 py-2 hover:bg-gray-100 cursor-pointer border-b"
+                                        onClick={() => {
+                                            navigate(`/${result.id}`);
+                                        }}
+                                    >
+                                        <div className=''>
+                                            {result.tenViTri}
+                                        </div>
                                     </div>
-                                </div>
-                            ))} */}
-                        </div>
+                                ))}
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
